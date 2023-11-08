43590c8bc0fc290f3608792bd2baa06e04601bfb
SmaginaEP <prinka7@rambler.ru>
2023-11-09 00:34:10 +0300

    fix linter problem

--- src/isEven.js
+++ src/isEven.js
@@ -10,18 +10,18 @@ const name = acquaintance();
 
 const isAnswerCorrect = () => {
   console.log('Answer "yes" if the number is even, otherwise answer "no".');
- 
+
   let answerCount = 0;
 
-  while (answerCount <= 2) { 
+  while (answerCount <= 2) {
     const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min) + min);
     const randomInt = getRandomInt(0, 500);
-    const isEven = (randomInt % 2 == 0) ? 'yes' : 'no';
+    const isEven = (randomInt % 2 === 0) ? 'yes' : 'no';
 
     console.log(`Question: ${randomInt}`);
 
     const answer = readlineSync.question('Your answer: ');
-    
+
     if (isEven === answer) {
       console.log('Correct!');
       answerCount += 1;
@@ -29,8 +29,8 @@ const isAnswerCorrect = () => {
     } else {
       console.log(`'${answer}' is wrong answer ;(. Correct answer was '${isEven}'.
             Let's try again, ${name}!`);
-    return;
-    } 
+      return;
+    }
   }
 
   console.log(`Congratulations, ${name}!`);
