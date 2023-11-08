063fc7f1cca3f76d05c4ea9f001d93af204328ce
SmaginaEP <prinka7@rambler.ru>
2023-11-08 22:27:22 +0300

    test npm link

--- bin/brain-even.js
+++ bin/brain-even.js
@@ -1,6 +1,4 @@
 #!/usr/bin/env node
-const getRandomInt = (min, max) => {
-  return Math.floor(Math.random() * (max - min) + min)
-};
+const getRandomInt = (min, max) => Math.floor(Math.random() * (max - min) + min);
 console.log(getRandomInt(0, 1000));
-getRandomInt(0, 1000);
+// getRandomInt(0, 1000);
