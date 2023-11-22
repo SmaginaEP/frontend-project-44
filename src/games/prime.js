eec996729346b2426adf2573f8d6df36f6622137
SmaginaEP <prinka7@rambler.ru>
2023-11-23 02:20:55 +0300

    added prime.js

---
+++ src/games/prime.js
@@ -0,0 +1,23 @@
+import getRandomInt from '../getRandomInt.js';
+import playGame from '../index.js';
+
+const rules = 'Answer "yes" if given number is prime. Otherwise answer "no".';
+
+const isPrime = () => {
+  const randomInt = getRandomInt(2, 20);
+  let rightAnswer = '';
+  for (let i = 2; i < randomInt; i++) {
+    if (randomInt % i === 0) {
+      rightAnswer = 'no';
+      break;
+    }
+    rightAnswer = 'yes';
+  }
+  console.log(rightAnswer);
+  const question = `${randomInt}`;
+  return [rightAnswer, question];
+};
+
+export default () => {
+  playGame(rules, isPrime);
+};
