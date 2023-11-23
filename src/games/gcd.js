32918e4269e307538445138436a3502b95718861
SmaginaEP <prinka7@rambler.ru>
2023-11-23 18:33:52 +0300

    fixed gcd.js

--- src/games/gcd.js
+++ src/games/gcd.js
@@ -4,8 +4,8 @@ import playGame from '../index.js';
 const rules = 'Find the greatest common divisor of given numbers.';
 
 const gcd = () => {
-  const randomInt1 = getRandomInt(0, 10);
-  const randomInt2 = getRandomInt(0, 10);
+  const randomInt1 = getRandomInt(1, 100);
+  const randomInt2 = getRandomInt(1, 100);
 
   const NOD = (randomInt1, randomInt2) => {
     if (randomInt2 > randomInt1) { return NOD(randomInt2, randomInt1); }
@@ -13,7 +13,7 @@ const gcd = () => {
     return NOD(randomInt2, randomInt1 % randomInt2);
   };
 
-  const rightAnswer = `${NOD}`;
+  const rightAnswer = `${NOD(randomInt1, randomInt2)}`;
   const question = `${randomInt1} ${randomInt2}`;
   return [rightAnswer, question];
 };
