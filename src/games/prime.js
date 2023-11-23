2b01b281e14eb8a06afc7acfa19058abca34fb49
SmaginaEP <prinka7@rambler.ru>
2023-11-23 19:18:42 +0300

    fixed prime.js

--- src/games/prime.js
+++ src/games/prime.js
@@ -4,14 +4,13 @@ import playGame from '../index.js';
 const rules = 'Answer "yes" if given number is prime. Otherwise answer "no".';
 
 const isPrime = () => {
-  const randomInt = getRandomInt(2, 20);
-  let rightAnswer;
+  const randomInt = getRandomInt(2, 500);
+  let rightAnswer = 'yes';
   for (let i = 2; i < randomInt; i++) {
     if (randomInt % i === 0) {
       rightAnswer = 'no';
       break;
     }
-    rightAnswer = 'yes';
   }
   const question = `${randomInt}`;
   return [rightAnswer, question];
